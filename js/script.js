a1aaf3d8d92308a1ea38f62d51f7ed4ac8517d6b
Guido Amodio <guido.amodio@gmail.com>
2020-07-29 17:35:00 +0200

    riceviamo un messaggio

--- js/script.js
+++ js/script.js
@@ -1,35 +1,78 @@
 $(document).ready(function() {
 
-    // var autoReply = [
-    //     '<p>io bene, tu?</p>',
-    //     '<p>grazie</p>',
-    //     '<p>ciao</p>',
-    //     '<p>è stato un piacere</p>'
-    // ]
+    var autoReply = [
+        '<p>io bene, tu?</p>',
+        '<p>grazie</p>',
+        '<p>ciao</p>',
+        '<p>è stato un piacere</p>'
+    ]
 
-// function()
-//     $('.template .message').clone();
-//     messYou.addClass(sent);
-//
 
     $('#my-Mess').keydown(send);
 
     function send(){
-        if (event.which == 13 || event.keydown == 13) {
+        if (event.which == 13 || event.keydown == 13 ) {
             var messaggio = $('#my-Mess').val();
-        }
-        var sendNow = $('.template .message').clone();
-        sendNow.prepend('<p>' + messaggio + '</p>');
-        sendNow.addClass('sent');
+            if(messaggio!=""){
+                var sendNow = $('.template .message').clone();
+                var timeNow = time();
+                sendNow.append('<p>' + messaggio + '</p>');
+                sendNow.append('<p>' + timeNow + '</p>');
+                sendNow.addClass('sent');
+                $('.chat .overlay').append(sendNow);
+                document.getElementById("my-Mess").value="";
 
-        $('.chat .overlay').append(sendNow);
+                setTimeout (function() {
+                    receiveNow = $('.template .message').clone();
+                    var tuoMessaggio = autoReply[getRandom(autoReply.length, 0)];
+                    receiveNow.append('<p>' + tuoMessaggio + '</p>');
+                    receiveNow.append('<p>' + timeNow + '</p>');
+                    receiveNow.addClass('received');
+                    $('.chat .overlay').append(receiveNow);
+                },2000)
+            }
+        }
     }
+
+    // $('#my-Mess').keydown(sendMe);
+    //
+    //
+    //
+    //     if (event.which == 13 || event.keydown == 13 ) {
+    //         var sendNow = $('.template .message').clone();
+    //         var timeNow = time();
+    //         var messaggio = autoReply[getRandom(0, autoReply.length)];
+    //         sendNow.append('<p>' + messaggio + '</p>');
+    //         sendNow.append('<p>' + timeNow + '</p>');
+    //         sendNow.addClass('received');
+    //         $('.chat .overlay').append(sendNow);
+    //     }
+    //
+
+
+
 
 
 
 
 });
 
-function geTrandom(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
+function getRandom(min, max) {
+  return Math.floor(Math.random()*(max - min))+min;
+}
+
+function addZero(i) {
+ if (i < 10) {
+   i = "0" + i;
+ }
+ return i;
+}
+
+function time() {
+    var d = new Date();
+    var h = addZero(d.getHours());
+    var m = addZero(d.getMinutes());
+    var x = h + ':' + m;
+ return x;
+
 }
